611f7f508f154c6ad9f881fba1eb027dea800243
Hudson Finn <hkfinn@tidewater.net>
2023-06-15 19:17:43 -0400

    Colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,10 +3,6 @@ return {
     name = 'catppuccin',
     opts = {
         flavour = 'mocha',
-        color_overrides = require('config.style').catppuccin_overrides,
-        custom_highlights = require('config.style').vitesse_highlights,
-
-        term_colors = true,
         dim_inactive = { enabled = true, shade = 'dark', percentage = 0.45 },
     },
 }
