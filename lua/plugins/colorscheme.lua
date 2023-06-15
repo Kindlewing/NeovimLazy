0b2c55bc22ac66b97225832bf54c8ece71a25694
Hudson Finn <hkfinn@tidewater.net>
2023-06-15 15:50:35 -0400

    Tailwind

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,26 +1,26 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-      flavour = "mocha",
-      background = {
-        light = "latte",
-      },
-
-      color_overrides = require("config.style").catppuccin_overrides,
-      custom_highlights = require("config.style").vitesse_highlights,
-
-      term_colors = true,
-      dim_inactive = { enabled = true, shade = "dark", percentage = 0.45 },
-    },
-
-    -- Configure LazyVim to load colorscheme
     {
-      "LazyVim/LazyVim",
-      opts = {
-        colorscheme = "catppuccin",
-      },
+        "catppuccin/nvim",
+        name = "catppuccin",
+        opts = {
+            flavour = "mocha",
+            background = {
+                light = "latte",
+            },
+
+            color_overrides = require("config.style").catppuccin_overrides,
+            custom_highlights = require("config.style").vitesse_highlights,
+
+            term_colors = true,
+            dim_inactive = { enabled = true, shade = "dark", percentage = 0.45 },
+        },
+
+        -- Configure LazyVim to load colorscheme
+        {
+            "LazyVim/LazyVim",
+            opts = {
+                colorscheme = "catppuccin",
+            },
+        },
     },
-  },
 }
