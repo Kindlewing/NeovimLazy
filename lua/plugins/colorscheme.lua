341fa0d58684a03e59bfe95e4a0245af051df7df
Hudson Finn <hkfinn@tidewater.net>
2023-06-15 19:08:30 -0400

    formatting

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,26 +1,12 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        opts = {
-            flavour = "mocha",
-            background = {
-                light = "latte",
-            },
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    opts = {
+        flavour = 'mocha',
+        color_overrides = require('config.style').catppuccin_overrides,
+        custom_highlights = require('config.style').vitesse_highlights,
 
-            color_overrides = require("config.style").catppuccin_overrides,
-            custom_highlights = require("config.style").vitesse_highlights,
-
-            term_colors = true,
-            dim_inactive = { enabled = true, shade = "dark", percentage = 0.45 },
-        },
-
-        -- Configure LazyVim to load colorscheme
-        {
-            "LazyVim/LazyVim",
-            opts = {
-                colorscheme = "catppuccin",
-            },
-        },
+        term_colors = true,
+        dim_inactive = { enabled = true, shade = 'dark', percentage = 0.45 },
     },
 }
