5d5aee6269d26cf73c57fb6e5f8262f4be3de678
Hudson Finn <hkfinn@tidewater.net>
2023-06-16 06:38:25 -0400

    Core

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,35 +1,35 @@
 return {
-  "williamboman/mason.nvim",
-  cmd = "Mason",
-  keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-  opts = {
-    ensure_installed = {
-      "stylua",
-      "shellcheck",
-      "editorconfig-checker",
-      "beautysh",
-      "marksman",
-      "shfmt",
-      "flake8",
-      "prettier",
-      "prettierd",
-      "rust-analyzer",
-      "pyright",
-      "rustfmt",
-      "typescript-language-server",
-      "tailwindcss-language-server",
-      "css-lsp",
-    },
-  },
-  ---@param opts MasonSettings | {ensure_installed: string[]}
-  config = function(plugin, opts)
-    require("mason").setup(opts)
-    local mr = require("mason-registry")
-    for _, tool in ipairs(opts.ensure_installed) do
-      local p = mr.get_package(tool)
-      if not p:is_installed() then
-        p:install()
-      end
-    end
-  end,
+	'williamboman/mason.nvim',
+	cmd = 'Mason',
+	keys = { { '<leader>cm', '<cmd>Mason<cr>', desc = 'Mason' } },
+	opts = {
+		ensure_installed = {
+			'stylua',
+			'shellcheck',
+			'editorconfig-checker',
+			'beautysh',
+			'marksman',
+			'shfmt',
+			'flake8',
+			'prettier',
+			'prettierd',
+			'rust-analyzer',
+			'pyright',
+			'rustfmt',
+			'typescript-language-server',
+			'tailwindcss-language-server',
+			'css-lsp',
+		},
+	},
+	---@param opts MasonSettings | {ensure_installed: string[]}
+	config = function(plugin, opts)
+		require('mason').setup(opts)
+		local mr = require('mason-registry')
+		for _, tool in ipairs(opts.ensure_installed) do
+			local p = mr.get_package(tool)
+			if not p:is_installed() then
+				p:install()
+			end
+		end
+	end,
 }
