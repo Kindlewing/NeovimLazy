8a00f416e56d2695f3afaaa6e01a3647ffdc2d1c
Kindlewing <hudson.finn@zohomail.com>
2023-07-09 10:52:36 -0400

    icons for mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,6 +3,13 @@ return {
 	cmd = 'Mason',
 	keys = { { '<leader>cm', '<cmd>Mason<cr>', desc = 'Mason' } },
 	opts = {
+		ui = {
+			icons = {
+				package_installed = '✓',
+				package_pending = '➜',
+				package_uninstalled = '✗',
+			},
+		},
 		ensure_installed = {
 			'stylua',
 			'shellcheck',
